dcf6bfd657efc286c20ccd921287f792bc4e13d5
Gabriel Haddad <gabriel.haddad@dtidigital.com.br>
2019-11-02 20:15:16 -0300

    corrige campo de seleção na pagina

---
+++ WebApp/Scripts/TabelaUsuario/listarTabela.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    ocultarTabelas();
+    $("#tabela-nf-anp").show();
+
+    $("#selecao-tipo-tabela").on("change", function (event) {
+        let valorCampo = $("#selecao-tipo-tabela").val();
+        ocultarTabelas();
+
+        if (valorCampo === "nf-anp") 
+            $("#tabela-nf-anp").show();
+
+        else if (valorCampo === "cupom") 
+            $("#tabela-cupom").show();
+
+        else if (valorCampo === "outros")
+            $("#tabela-outros").show();
+        
+    });
+
+    function ocultarTabelas() {
+        $("#tabela-nf-anp").hide();
+        $("#tabela-cupom").hide();
+        $("#tabela-outros").hide();
+    }
+
+});
