66659ef3d97652d471cc49ddfe39922adcfb2cda
Gabriel Haddad <gabriel.haddad@dtidigital.com.br>
2019-11-03 13:36:59 -0300

    corrige importações de arquivos css e js

---
+++ WebApp/Scripts/externo/data-table/data-table-act.js
@@ -0,0 +1,10 @@
+(function ($) {
+ "use strict";
+	
+	$(document).ready(function() {
+		 $('#data-table-nf-anp').DataTable();
+		 $('#data-table-cupom').DataTable();
+		 $('#data-table-outros').DataTable();
+	});
+ 
+})(jQuery); 
